93b2fcc5781f8f5be491953331e0385b4f8abd8e
Jau-Tzuoo Chen <robertfromsms@gmail.com>
2019-05-20 22:26:06 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,5 +1,6 @@
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
 def alphabetize(arr)
-  ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   arr.sort_by {|sentence|
     ESPERANTO_ALPHABET.index(sentence[0])
   }
